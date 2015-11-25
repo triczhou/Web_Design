8c8a1f4f20668dbe9345dd6e51ad3edde0e9302a
maxio110 <maxio.wang@gmail.com>
2015-11-25 00:29:12 -0500

    Session Authentication Completed
    Username: admin
    password: 123
    Login to add/delete information now

--- js/main.js
+++ js/main.js
@@ -3,37 +3,47 @@ var app = angular.module("NYUsdg",[]);
 app.controller("SdgController", 
 	function ($scope, $http){
 		$scope.method = 'GET';
-		$scope.url = 'http://websys3.stern.nyu.edu:7003/nyusdg/store/';
+		$scope.url = 'http://websys3.stern.nyu.edu:7003/';
+		//$scope.url = 'http://localhost:7003/';
 		/*
 		$http.get("websys3.stern.nyu.edu:7003/nyusdg/store/")
 		.success(function (response){
 				$scope.stores = response;
 			});
 			*/
-		
-		
-		$http({method: $scope.method, url: $scope.url}).
+		$http({method: $scope.method, url: $scope.url + 'store/'}).
 			then(function(response) {
 				$scope.data = response.data;
 			}, function(response) {
-				alert("Request failed");
+				alert(response.data);;
 		});
 		$scope.remove = function(id){
 			//alert(id);
-			$http.delete($scope.url + id).
+			$http.delete($scope.url +'store/'+ id).
 			then(function (response){
-					$scope.data = response.data;
+				if(response.data == '1'){alert('Please login');}
+				else{$scope.data = response.data;}
 				}, function (response){
-					alert("Delete failed");
+					alert(response.data);;
 			});
 		};
 		$scope.addNew = function(){
         	var tmp = $scope.fields;
-        	$http.post($scope.url, tmp).
+        	$http.post($scope.url+'store/', tmp).
 			then(function(response) {
-					$scope.data = response.data;
+				if(response.data == '1'){alert('Please login');}
+				else{$scope.data = response.data;}
 				}, function(response) {
-					alert("AddNew failed");
+					alert('Unknown Error');;
+			});
+    	};
+    	$scope.login = function(){
+        	var tmp = $scope.info;
+        	$http.post($scope.url+'login/', tmp).
+			then(function(response) {
+				alert(response.data);
+				}, function(response) {
+					alert('Unknown Error');
 			});
     	};
 	});
